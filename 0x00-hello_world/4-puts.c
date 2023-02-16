86ae1c0c27a8e992e3bf9c60387c0818a9c97a10
Kivashan <bvkivashan1982@gmail.com>
2023-02-16 15:47:25 -0800

    $commitMessage

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *Main - Entry point
+ *Prints string 
+ *
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
