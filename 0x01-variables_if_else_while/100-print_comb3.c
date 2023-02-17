d036c90dc072fd73e0fc19e15303885bf34e314c
Kivashan <bvkivashan1982@gmail.com>
2023-02-17 14:36:48 -0800

    $commitMessage

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - Entry poit
+ *
+ *Return : Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int tmp = 0;
+
+	while (i <= 99)
+	{
+		tmp = i / 10;
+		putchar(tmp + 48);
+		tmp = i % 10;
+		putchar(tmp + 48);
+		if (i == 99)
+			break;
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
