60e0e755dd2f91a7666d1618d6dd4b9a5bbaae2e
Kivashan <bvkivashan1982@gmail.com>
2023-02-17 14:12:29 -0800

    $commitMessage

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,24 +7,17 @@
 
 int main(void)
 {
-	char i = '0';
-	char arr[] = "0, ";
+	int i = 0;
 
-	do {
-		int j = 0;
-
-		arr[0] = i;
-		if (i < '9')
-		{
-			while (arr[j] != '\0')
-			{
-				putchar(arr[j]);
-				j++;
-			}
-		}
-	i++;
-	} while (i <= '9');
-	putchar('9');
+	while (i <= 9)
+	{
+		putchar(i + 48);
+		if (i == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+		i++;
+	}
 	putchar('\n');
 	return (0);
 }
