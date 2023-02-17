f34848975d887435aff41a6f15c70011d320f60c
Kivashan <bvkivashan1982@gmail.com>
2023-02-16 18:16:42 -0800

    $commitMessage

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i = 0;
 	char array[] = "and that piece of art is useful\" - Dora Korpar,\
-		       	2015-10-19\n";
+	2015-10-19\n";
 
-	do{
+	do {
 		putchar(array[i]);
 		++i;
 	} while (array[i] != '\0');
