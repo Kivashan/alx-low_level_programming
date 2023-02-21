a7d844e2712753d13dee424711546a377dff321d
Kivashan <bvkivashan1982@gmail.com>
2023-02-21 00:00:07 -0800

    $commitMessage

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+int _putchar(char c);
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char arr[] = "_putchar";
+	int i = 0;
+
+	while (arr[i] != '\0')
+	{
+		_putchar(arr[i]);
+		i++;
+	}
+	_putchar('\n');
+	return 0;
+}
