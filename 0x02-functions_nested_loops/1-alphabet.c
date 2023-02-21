92efc2f30c59dc732c6ed84012107d18321384a0
Kivashan <bvkivashan1982@gmail.com>
2023-02-21 11:08:38 -0800

    $commitMessage

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+/**
+ *print_alphabet - Entry point
+ *
+ *Return: Void
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return;
+}
