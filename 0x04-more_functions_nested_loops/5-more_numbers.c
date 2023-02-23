a94955a5f5474f90d8f5d49588fba9ccad98c3ef
Kivashan <bvkivashan1982@gmail.com>
2023-02-23 12:00:06 -0800

    $commitMessage

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *more_numbers - Entry point
+ *
+ *Return: Void
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int b = 0;
+
+	while (i <= 14)
+	{
+		while (b <= 14)
+		{
+			if (b >= 10)
+			{
+				_putchar(b / 10 + 48);
+				_putchar(b % 10 + 48);
+			}
+			else
+				_putchar(b % 10 + 48);
+			b++;
+		}
+		_putchar('\n');
+		i++;
+		b = 0;
+	}
+}
