a0bfa1550aeec5e82847b0be714fb8e68aa9158f
Kivashan <bvkivashan1982@gmail.com>
2023-02-23 12:09:29 -0800

    $commitMessage

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@ void more_numbers(void)
 	int i = 0;
 	int b = 0;
 
-	while (i <= 14)
+	while (i < 14)
 	{
 		while (b <= 14)
 		{
-			if(b >= 10)
+			if (b >= 10)
 				_putchar(b / 10 + 48);
 			_putchar(b % 10 + 48);
 			b++;
