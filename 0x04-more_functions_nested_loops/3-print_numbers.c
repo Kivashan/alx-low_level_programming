9055461aba2937962bb1dceb70478efcf079f36e
Kivashan <bvkivashan1982@gmail.com>
2023-02-23 11:30:01 -0800

    $commitMessage

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_numbers - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	char a = 48;
+
+	while (a <= '57')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
