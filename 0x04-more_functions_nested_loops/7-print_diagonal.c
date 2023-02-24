57672d75d8101062ad53411bc50dab783e69c82e
Kivashan <bvkivashan1982@gmail.com>
2023-02-24 09:47:36 -0800

    $commitMessage

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *print_diagonal - Entry point
+ *@n: integer argument
+ *
+ *Return: Void
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 1;
+	int tmp;
+
+	while (i <= n && n != 0)
+	{
+		tmp = 0;
+		while (tmp <= j)
+		{
+			_putchar(' ');
+			if (tmp == j)
+				_putchar('\\');
+			tmp++;
+		}
+		_putchar('\n');
+		i++;
+		j++;
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
