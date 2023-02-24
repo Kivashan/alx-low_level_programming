2dd959287d99e64d15d9c13c3e54daf67c6f9099
Kivashan <bvkivashan1982@gmail.com>
2023-02-24 10:17:38 -0800

    $commitMessage

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *print_square - Entry point
+ *@size: Integer argument
+ *
+ *Return: Void
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < size)
+	{
+		while(j <= size)
+		{
+			if (j == size)
+				_putchar('\n');
+			else
+				_putchar('#');
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
