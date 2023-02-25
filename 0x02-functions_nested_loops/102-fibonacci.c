09946d2a393ef7d2c214f4dfa4ce722a071180d6
Kivashan <bvkivashan1982@gmail.com>
2023-02-25 12:44:14 -0800

    $commitMessage

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int a = 1;
+	long int b = 2;
+	long int tmp;
+	long int count;
+
+	printf("1, 2, ");
+
+	for (count = 0; count < 48; count++)
+	{
+		tmp = a;
+		a = b;
+		b = a + tmp;
+		printf("%ld, ", b);
+	}
+	printf("\n");
+}
