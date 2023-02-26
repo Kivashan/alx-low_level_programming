e3baf6f656af5476e9d3a92e0c604c679439d94d
Kivashan <bvkivashan1982@gmail.com>
2023-02-26 10:22:06 -0800

    $commitMessage

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,9 +9,14 @@
 void print_number(int n)
 {
 	int tmp = 0;
+	int count = 0;
 
-	if (n >= 0)
+	if (n < 0)
 	{
+		n = -n;
+		count = 1;
+	}
+
 		while (n >= 10)
 		{
 			tmp = (tmp * 10) + n % 10;
@@ -22,9 +27,12 @@ void print_number(int n)
 
 		while (tmp >= 10)
 		{
+			if (count == 1)
+			{
+				_putchar('-');
+			}
 			_putchar(tmp % 10 + 48);
 			tmp = tmp / 10;
 		}
 		_putchar(tmp + 48);
-	}
 }
