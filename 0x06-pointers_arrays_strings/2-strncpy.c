e73974f937c4bc88bc844933108776b3fb060d63
Kivashan <bvkivashan1982@gmail.com>
2023-03-01 00:19:26 -0800

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * strncpy - copies a string
+ * @dest: buffer for copy to be placed in
+ * @src: string to copy
+ * @n: the number of elements from src to be copied into buffer
+ *
+ * Return: pointer to a string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ char *_strcat(char *, char *);
 
 char *_strncat(char *, char *, int);
 
+char *_strncpy(char *, char *, int);
+
 #endif
