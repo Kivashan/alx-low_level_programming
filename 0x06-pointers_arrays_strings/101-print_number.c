6040f8c47ba4f6aba1e4bbf5b1b677331a12dcd4
Kivashan <bvkivashan1982@gmail.com>
2023-03-02 05:55:35 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,31 +9,26 @@
 
 void print_number(int n)
 {
-	unsigned int tmp = 0;
-	unsigned int tmp1 = n;
-	int last_digit = 0;
+	unsigned int tmp = n, tmp2 = n;
+	unsigned int count = 1;
 
-	if (n % 10 == 0 && n != 0)
-		last_digit = 1;
 	if (n < 0)
 	{
 		_putchar('-');
-		tmp1 = -n;
+		tmp2 = -n;
 	}
 
-		while (tmp1 > 9)
-		{
-			tmp = (tmp * 10) + tmp1 % 10;
-			tmp1 = tmp1 / 10;
-		}
-		tmp = (tmp * 10) + tmp1 % 10;
-		tmp1 = 0;
-		while (tmp > 9)
-		{
-			_putchar(tmp % 10 + 48);
-			tmp = tmp / 10;
-		}
-		_putchar(tmp + 48);
-		if (last_digit == 1)
-			_putchar('0');
+	while (tmp2 > 9)
+	{
+		tmp2 = tmp2 / 10;
+		count *= 10;
+	}
+
+	while (count > 9)
+	{
+		_putchar((tmp / count) % 10 + 48);
+		count /= 10;
+
+	}
+	_putchar(tmp % 10 + 48);
 }
