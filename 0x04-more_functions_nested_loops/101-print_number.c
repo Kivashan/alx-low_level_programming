901c2999c2a16617e7a7fbee699d2198cf26ce4a
Kivashan <bvkivashan1982@gmail.com>
2023-03-04 06:01:18 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,30 +8,31 @@
 
 void print_number(int n)
 {
-	int tmp = 0;
-	int last_digit = 0;
+	unsigned int tmp, tmp2, count;
 
-	if (n % 10 == 0 && n != 0)
-		last_digit = 1;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		tmp2 = -n;
+		tmp = -n;
+	}
+	else
+	{
+		tmp2 = n;
+		tmp = n;
 	}
 
-		while (n > 9)
-		{
-			tmp = (tmp * 10) + n % 10;
-			n = n / 10;
-		}
-		tmp = (tmp * 10) + n % 10;
-		n = 0;
-		while (tmp > 9)
-		{
-			_putchar(tmp % 10 + 48);
-			tmp = tmp / 10;
-		}
-		_putchar(tmp + 48);
-		if (last_digit == 1)
-			_putchar('0');
+	count = 1;
+	while (tmp2 > 9)
+	{
+		tmp2 = tmp2 / 10;
+		count *= 10;
+	}
+
+	while (count > 9)
+	{
+		_putchar((tmp / count) % 10 + 48);
+		count /= 10;
+	}
+	_putchar(tmp % 10 + 48);
 }
