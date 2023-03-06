3bb64baaaf69d1587c16e1d8de8b87fab1f56bc7
Kivashan <bvkivashan1982@gmail.com>
2023-03-06 03:26:20 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - returns first occurrence of c if found, else NULL
+ * @s - source string
+ * @c - character to search for
+ *
+ * Return: returns 1st occurence of c if found else NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*++s == c)
+			return (s);
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,6 @@ char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *, char *, unsigned int);
 
+char *_strchr(char *s, char c);
+
 #endif
