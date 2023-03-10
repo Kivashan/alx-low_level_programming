55094566a318a326213696a75f621b3be04651d2
Kivashan <bvkivashan1982@gmail.com>
2023-03-10 10:23:06 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector - pointer to an array of strings
+ *
+ * Return: 0 Always (success)
+ */
+
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
