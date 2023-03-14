c8319f03ee5be60b0476a4332766f8f0ffe80508
Kivashan <bvkivashan1982@gmail.com>
2023-03-14 00:26:35 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 	int j;
 
 	if (s1 == NULL && s2 == NULL)
-		len = 0;
+		return (NULL);
 	else if (s1 == NULL)
 		len = str_length(s2) + 1;
 	else if (s2 == NULL)
