65555a7e95a26839d368e35d0000752f2ad307a6
Kivashan <bvkivashan1982@gmail.com>
2023-03-14 00:06:14 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdlib.h>
+
+int str_length(char *s1, char *s2);
+
+/**
+ * str_concat - allocates new memory for a string and stores s1 and s2 in new
+ * memory as a concatenated string
+ * @s1: pointer to a string to be copied
+ * @s2: pointer to a string to be copied and concatenated to s1
+ *
+ * Return: Null if malloc fails, pointer to new memory area if success
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len = 0;
+	char *array;
+	int i;
+	int j;
+
+	len = str_length(s1, s2);
+
+	/* allocate memory */
+	array = malloc(sizeof(char) * len);
+
+	/* copy contents of s1 and s2 to array */
+	for (i = 0; i < len && s1[i] != '\0'; i++)
+		array[i] = s1[i];
+
+	for (j = 0; s2[j] != '\0'; j++)
+		array[i + j] = s2[j];
+
+	array[i + j] = '\0';
+
+	return (array);
+}
+
+/**
+ * str_length - get length of s1 and s2
+ * @s1: string
+ * @s2: string
+ *
+ * Return: length of string
+ */
+
+int str_length(char *s1, char *s2)
+{
+	int len = 0;
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		len++;
+
+	for (i = 0; s2[i] != '\0'; i++)
+		len++;
+
+	return (len + 1);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,5 +4,6 @@
 int _putchar(char);
 char *create_array(unsigned int, char);
 char *_strdup(char *);
+char *str_concat(char *, char *);
 
 #endif
