723aed7c8f135bd182d22fa180382c6b8aa63307
Kivashan <bvkivashan1982@gmail.com>
2023-03-16 17:15:00 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,8 +19,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	/*checks for arguments */
 	if (new_size == 0 && ptr)
+	{
+		free(ptr);
 		return (NULL);
-
+	}
 	if (new_size == old_size)
 		return (ptr);
 
