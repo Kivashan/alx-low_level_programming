fd6d55917c65c9ea1859a68b21d77c3458bd0f6d
Kivashan <bvkivashan1982@gmail.com>
2023-03-20 10:06:33 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -11,9 +11,9 @@ int main(void)
 {
     int i;
     int j;
-
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
+	int x = 0;
+    i = ABS(x) * 10;
+    j = ABS(--x) * 10;
     printf("%d, %d\n", i, j);
     return (0);
 }
