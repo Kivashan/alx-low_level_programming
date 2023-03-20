d29f887dc636351adbaada801eb90e69e2adae1c
Kivashan <bvkivashan1982@gmail.com>
2023-03-20 12:40:46 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data fields containing data on a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dogs owner
+ *
+ * Description: Structure containing information pertaining
+ * to a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
