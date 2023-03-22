6845fa44c2623c673b5ecaba5b47ca0fc816efd9
Kivashan <bvkivashan1982@gmail.com>
2023-03-22 05:20:55 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - returns an index for the first match from cmp function
+ * @array: pointer to an array of values to cmp
+ * @size: size of the array
+ * @cmp: pointer to a compare function
+ *
+ * Return: -1 if no matches or fail, positive integer otherwise
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || size <= 0 || !cmp)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+
+	return (-1);
+}
