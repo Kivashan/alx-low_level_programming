c92436fcec0411a27a5727a89a087ff4b6c23fca
Kivashan <bvkivashan1982@gmail.com>
2023-04-10 15:51:37 -0700

    1-print_binary.c revised version 4 - used bitwise & to check replacing unsigned long int

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,21 +10,13 @@
 void print_binary(unsigned long int n)
 {
 	int count = 0;
-	int flag = 0;
 
-	while ((unsigned long int)(1 << count) <= n / 2)
+	while ((n >> count) > 1)
 		count++;
 
 	while (count >= 0)
 	{
-		if ((unsigned long int)(1 << count) <= n)
-		{
-			n = n - (1 << count);
-			_putchar('1');
-			flag = 1;
-		}
-		else if (flag == 1 || n == 0)
-			_putchar('0');
+		_putchar((n >> count) & 1 ? '1' : '0');
 		count--;
 	}
 }
