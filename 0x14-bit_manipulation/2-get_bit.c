4e14180d5a1bab69df1265e280da77d6f3074ad2
Kivashan <bvkivashan1982@gmail.com>
2023-04-10 16:56:17 -0700

    2-get_bit.c - ammended check for index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,7 +15,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	while ((n >> count) > 1)
 		count++;
 
-	if (index > count || (index >= (sizeof(unsigned long int) * 8)))
+	if ((index > count) || (index >= (sizeof(unsigned long int) * 8)))
 		return (-1);
 
 	return ((n >> index) & 1 ? 1 : 0);
