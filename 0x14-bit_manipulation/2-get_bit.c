d9015f690a48235f3e3348a44ad01ffc33ef3f42
Kivashan <bvkivashan1982@gmail.com>
2023-04-10 17:14:36 -0700

    2-get_bit.c - ammended check for index - no need to track the last bit containing 1 as all bits after 1 will be 0s up until sizeof(unsigned long int) * 8

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,12 +10,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int count = 0;
-
-	while ((n >> count) > 1)
-		count++;
-
-	if ((index > count) || (index >= (sizeof(unsigned long int) * 8)))
+	if ((index >= (sizeof(unsigned long int) * 8)))
 		return (-1);
 
 	return ((n >> index) & 1 ? 1 : 0);
