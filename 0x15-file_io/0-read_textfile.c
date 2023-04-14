b21d56d5ed84efaf1c96f9a3c90d1bec672fa2b9
Kivashan <bvkivashan1982@gmail.com>
2023-04-14 01:08:19 -0700

    0-read_textfile.c - changed flag from 0_RDWR to 0-RDONLY

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buffer || !filename)
 		return (0);
 
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY);
 	/*checks if open was successful */
 	if (fd != -1)
 	{
