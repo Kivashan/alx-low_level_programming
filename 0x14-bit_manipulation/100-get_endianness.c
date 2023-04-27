7ac98900cbf881e521b5b62fe5f75addca104c4e
Kivashan <bvkivashan1982@gmail.com>
2023-04-27 13:57:34 -0700

    Resolved betty issues

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+ * get_endianness - gets endianness
+ *
+ * Return: 0 for big endian and 1 for little endian
+ */
+
 int get_endianness(void)
 {
 	unsigned int i;
 	char *ch;
 
 	i = 1;
-	ch = ((char*) &i);
+	ch = ((char *) &i);
 
 	if (ch)
 		return (1);
