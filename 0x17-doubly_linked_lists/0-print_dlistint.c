d8dc8259b0342862be83a9fbeab4f961b3b19dfd
Kivashan <bvkivashan1982@gmail.com>
2023-05-18 16:54:56 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints the data stored in a doubly linked list
+ * @h: head node of list
+ *
+ * Return: the number of nodes in the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	int nodes = 0;
+	/* check if h is null*/
+	if (!h)
+		return (nodes);
+	temp = h;
+
+	/* traverse through list*/
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		nodes++;
+		temp = temp->next;
+	}
+
+	return (nodes);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -16,5 +20,7 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
 
 #endif
