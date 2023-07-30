682fc41dd933ddd7b0fe5bfecc06ff5bfb92ed17
Kivashan <bvkivashan1982@gmail.com>
2023-07-30 14:34:21 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,6 +40,6 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_node_t *create_hash_node(const char *key, const char *value);
-/* int is_new_key(hash_table_t *ht, int index, const char *key); */
+int is_new_key(hash_table_t *ht, int index, const char *key);
 
 #endif
