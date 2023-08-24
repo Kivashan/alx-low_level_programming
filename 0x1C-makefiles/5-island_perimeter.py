fc713faddc1ecfb3b05dd1b80fadb77b68f7ae7c
Kivashan <bvkivashan1982@gmail.com>
2023-08-23 17:48:10 -0700

    Accounted for range in task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -25,19 +25,19 @@ def island_perimeter(grid):
             if grid[row][column] == 1:
 
                 # check whats north of current element
-                if (row - 1 >= 0) and grid[row - 1][column] == 0:
+                if (row - 1 > 0) and grid[row - 1][column] == 0:
                     cells += 1
 
                 # check whats south of current element
-                if (row + 1 <= r) and grid[row + 1][column] == 0:
+                if (row + 1 < r) and grid[row + 1][column] == 0:
                     cells += 1
 
                 # check whats east of current element
-                if (column + 1 <= c) and grid[row][column + 1] == 0:
+                if (column + 1 < c) and grid[row][column + 1] == 0:
                     cells += 1
 
                 # check whats west of current element
-                if (column - 1 >= 0) and grid[row][column - 1] == 0:
+                if (column - 1 > 0) and grid[row][column - 1] == 0:
                     cells += 1
 
     return (cells)
